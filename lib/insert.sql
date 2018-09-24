49c0ebff0d90993c1fb941d9c04fafff8fbbdf89
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:48:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,4 @@
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr. Chocolate",20,"male","blue");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",10,"male","black");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",6,"female","white");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",3,"male","dark brown");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa","female",13,"yellow");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",2,"male","pink");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",6,"female","grey");
--- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(null);
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "M", "black", "intense", 1),
