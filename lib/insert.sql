1d8279bce92b389cfafc9ea6223059a6a42f83cc
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:38:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,17 @@
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr. Chocolate",20,"male","blue");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",10,"male","black");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",6,"female","white");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",3,"male","dark brown");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa","female",13,"yellow");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",2,"male","pink");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",6,"female","grey");
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(null);
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr. Chocolate",20,"male","blue");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",10,"male","black");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",6,"female","white");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",3,"male","dark brown");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa","female",13,"yellow");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",2,"male","pink");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",6,"female","grey");
+-- INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(null);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
