c09aea301753cda53f436405f0985e479feed003
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:30:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Mr. Chocolate",20);
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Rowdy",10);
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Tabitha",6);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Sergeant Brown",3);
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Sergeant Brown",3,"male","dark brown");
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Melissa",13);
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Grinch",2);
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Wendy",6);
