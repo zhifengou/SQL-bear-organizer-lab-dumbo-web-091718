16c298da97fa5b2ed4d42b529c05e5e32ba59908
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:31:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Mr. Chocolate",20);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Rowdy",10);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Tabitha",6);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Sergeant Brown",3,"male","dark brown");
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Melissa",13);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Grinch",2);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Wendy",6);
-INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES(null);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr. Chocolate",20);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",10);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",6);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",3,"male","dark brown");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa",13);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",2);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",6);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(null);
