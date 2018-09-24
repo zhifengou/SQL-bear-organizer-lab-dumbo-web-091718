05e0defff0a4618f21d28d3b506f8cbb3926108c
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 16:23:30 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name FROM bears WHERE "
+  "SELECT id,name,age,gender, FROM bears WHERE "
 end
