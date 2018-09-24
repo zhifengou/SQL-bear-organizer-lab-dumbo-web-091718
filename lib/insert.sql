84b0a603b8d194339d03f34d25872b9b9e5e0047
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:22:36 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears(name) VALUES("Mr. Chocolate");
-INSERT INTO bears(name) VALUES("Rowdy");
-INSERT INTO bears(name) VALUES("Tabitha");
+INSERT INTO bears(name TEXT,age INTEGER,gender TEXT,color TEXT,temperament TEXT,alive BOOLEAN) VALUES("Mr. Chocolate");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Rowdy");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Tabitha");
 INSERT INTO bears(name) VALUES("Sergeant Brown");
 INSERT INTO bears(name) VALUES("Melissa");
 INSERT INTO bears(name) VALUES("Grinch");
