e643d0054e1d036f4f8c57eda210be6bbaf4d1a4
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:15:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears(name) VALUES("Tabitha");
 INSERT INTO bears(name) VALUES("Sergeant Brown");
 INSERT INTO bears(name) VALUES("Melissa");
 INSERT INTO bears(name) VALUES("Grinch");
-INSERT INTO bears(name) VALUES("Mr. Chocolate");
-INSERT INTO bears(name) VALUES("Mr. Chocolate");
+INSERT INTO bears(name) VALUES("Wendy");
+INSERT INTO bears(name) VALUES("");
