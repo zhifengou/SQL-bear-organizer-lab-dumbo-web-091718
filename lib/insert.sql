e3da5e9d7c1e9d6ff67c982a6caf7a6685d3274d
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 15:23:03 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name TEXT,age INTEGER,gender TEXT,color TEXT,temperament TEXT,alive BOOLEAN) VALUES("Mr. Chocolate");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Mr. Chocolate");
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Rowdy");
 INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Tabitha");
-INSERT INTO bears(name) VALUES("Sergeant Brown");
-INSERT INTO bears(name) VALUES("Melissa");
-INSERT INTO bears(name) VALUES("Grinch");
-INSERT INTO bears(name) VALUES("Wendy");
-INSERT INTO bears(name) VALUES(null);
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Sergeant Brown");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Melissa");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Grinch");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES("Wendy");
+INSERT INTO bears(name,age,gender,color,temperament,alive,alive) VALUES(null);
